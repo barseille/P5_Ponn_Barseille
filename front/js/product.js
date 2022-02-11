ffa390820f89f4a89b1c4abd396baa93da5b104e
BarseillePonn <ponn.barseille@gmail.com>
2022-02-11 16:45:44 +0100

    page product.js terminée

--- front/js/product.js
+++ front/js/product.js
@@ -1,42 +1,44 @@
-let url = new URL(window.location.href);
-let idProduct = url.searchParams.get("id");
+const url = new URL(window.location.href);
+const idProduct = url.searchParams.get("id");
 const UrlProduct = `http://localhost:3000/api/products/${idProduct}`;
 
 const colors = document.getElementById("colors");
 const itemQty = document.getElementById("quantity");
+let myProduct = {};
+
+//appel des éléments depuis l'API
+const image = document.querySelector(".item__img");
+const titre = document.querySelector("#title");
+const prix = document.querySelector("#price");
+const description = document.querySelector("#description");
+
+function optionColors(colors) {
+  const select = document.querySelector("#colors");
+  colors.forEach((couleur) => {
+    const option = document.createElement("option");
+    option.textContent = couleur;
+    select.appendChild(option);
+  });
+}
 
 // appel de l'api
 //affiche des produits dans la page produit
 fetch(UrlProduct)
   .then((response) => response.json())
   .then((data) => {
-    dataProduct = data;
-
-    //appel des éléments depuis l'API
-    const image = document.querySelector(".item__img");
-    const titre = document.querySelector("#title");
-    const prix = document.querySelector("#price");
-    const description = document.querySelector("#description");
-
     //relié API et HTML
     image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-    imageUrl = data.imageUrl;
-    imageAlt = data.altTxt;
     titre.innerHTML = `${data.name}`;
     prix.innerHTML = `${data.price}`;
     description.innerHTML = `${data.description}`;
     const colors = data.colors;
     optionColors(colors);
 
-    // choix de couleurs
-    function optionColors(colors) {
-      const select = document.querySelector("#colors");
-      colors.forEach((couleur) => {
-        const option = document.createElement("option");
-        option.textContent = couleur;
-        select.appendChild(option);
-      });
-    }
+    // clé de l'objet myProduct
+    myProduct.id = data._id;
+    myProduct.name = data.name;
+    myProduct.imageUrl = data.imageUrl;
+    myProduct.altTxt = data.altTxt;
   })
   .catch(function (err) {
     console.log("Fetch Erreur");
@@ -46,7 +48,7 @@ fetch(UrlProduct)
   });
 
 const buttonPanier = document.querySelector("#addToCart");
-buttonPanier.addEventListener("click", (event) => {
+buttonPanier.addEventListener("click", () => {
   // création tableau vide
   let arrayItem = [];
 
@@ -54,22 +56,24 @@ buttonPanier.addEventListener("click", (event) => {
   let colorProduct = document.querySelector("#colors").value;
   let quantityProduct = document.querySelector("#quantity").value;
 
-  //dataProduct = data
-  const productId = dataProduct._id;
-  const nameProduct = dataProduct.name;
-  const imageUrlProduct = dataProduct.imageUrl;
-  const imageAltTxtProduct = dataProduct.altTxt;
-
   //créer un objet pour mettre dans le local storage
   let produitPanier = {
-    id: productId,
-    name: nameProduct,
+    id: myProduct.id,
+    name: myProduct.name,
     color: colorProduct,
-    quantity: parseInt(quantityProduct, 10), //rajoute une qantité décimale
-    img: imageUrlProduct,
-    alt: imageAltTxtProduct,
+    quantity: parseInt(quantityProduct, 10), //rajoute une quantité décimale
+    img: myProduct.imageUrl,
+    alt: myProduct.altTxt,
   };
   console.log(produitPanier);
+
+  if (produitPanier.quantity == 0) {
+    alert("Veuiller saisir une couleur et une quantité d'article");
+    return;
+  } else if (produitPanier.quantity < 0 || produitPanier.quantity > 100) {
+    alert("Veuillez indiquer un nombre d'article entre 1 et 100");
+    return;
+  }
 
   //rajoute le produit dans le local storage s'il y a déjà un produit dans le panier
   if (localStorage.getItem("panier")) {
@@ -85,25 +89,13 @@ buttonPanier.addEventListener("click", (event) => {
         //ajouter le produit dans le tableau remplit
         arrayItem[i].quantity = arrayItem[i].quantity + produitPanier.quantity;
 
-        if (arrayItem[i].quantity > 100) {
-          arrayItem[i].quantity = 100;
-          alert("Le nombre d'articles selectionnés est trop important !");
-        }
-
-        if (arrayItem[i].quantity <= 0) {
-          // ajouter option -1 ne doit pas se calculer
-          arrayItem[i].quantity <= 0;
-          alert("NON !");
-        }
         localStorage.setItem("panier", JSON.stringify(arrayItem));
         return;
       }
     }
-    arrayItem.push(produitPanier);
-    localStorage.setItem("panier", JSON.stringify(arrayItem));
-  } // si le panier est vide, ajoute le product directement
-  else {
-    arrayItem.push(produitPanier);
-    localStorage.setItem("panier", JSON.stringify(arrayItem));
   }
+
+  // si le panier est vide, ajoute le product directement
+  arrayItem.push(produitPanier);
+  localStorage.setItem("panier", JSON.stringify(arrayItem));
 });
